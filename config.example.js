f939b71632c9056f2bdafee2ca38215717739656
Trịnh Anh Khoa <anhkhoa1612vn@gmail.com>
2026-07-23 13:09:01 +0700

    Ẩn link Google Sheets cá nhân bằng config.js và .gitignore

---
+++ config.example.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    // Dán đường dẫn Google Sheets của bạn vào đây và đổi tên file này thành config.js
+    GOOGLE_SHEET_URL: "https://docs.google.com/spreadsheets/d/1Z8P9F3b_67Wz_oGg06q9XpTchfE92BfK7pWswmQJpA8/edit?usp=sharing"
+};
